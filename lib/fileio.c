19ab96c696178031a787003261bcb42c7f0664cc
bbshelper <bbshelper@gmail.com>
2009-01-16 14:19:43 +0000

    Fixed warnings in fileio.c fileio.c: In function 'file_append': fileio.c:55: warning: implicit declaration of function 'flock' fileio.c:56: warning: implicit declaration of function 'lseek' fileio.c:57: warning: implicit declaration of function 'write' fileio.c:59: warning: implicit declaration of function 'close' fileio.c: In function 'f_cp': fileio.c:120: warning: implicit declaration of function 'read' fileio.c: In function 'f_ln': fileio.c:136: warning: implicit declaration of function 'link' fileio.c: In function 'f_rm': fileio.c:173: warning: implicit declaration of function 'unlink' fileio.c: In function 'rm_dir': fileio.c:191: warning: suggest parentheses around assignment used as truth value fileio.c:199: warning: suggest parentheses around assignment used as truth value fileio.c:214: warning: implicit declaration of function 'rmdir'

--- lib/fileio.c
+++ lib/fileio.c
@@ -1,48 +1,13 @@
-/*
- * fileio.c		-- some stuff that file/io related
- *
- * of SEEDNetBBS generation 1 (libtool implement)
- *
- * Copyright (c) 1999, Edward Ping-Da Chuang <edwardc@edwardc.dhs.org>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * CVS: $Id: fileio.c 366 2007-05-12 16:35:51Z danielfree $
- */
-
-#ifdef BBS
-#include "bbs.h"
-#else
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <string.h>
-#include <errno.h>
-#include <sys/stat.h>
+#include <fcntl.h>
 #include <dirent.h>
-#endif
+#include <unistd.h>
+#include <errno.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-char fileio_c[] = "$Id: fileio.c 366 2007-05-12 16:35:51Z danielfree $";
 #define        BLK_SIZ         4096
 
 static int rm_dir();
@@ -188,7 +153,7 @@ static int rm_dir(char *fpath) {
 	if (!(dirp = opendir(fpath)))
 		return -1;
 
-	for (fname = buf; *fname = *fpath; fname++, fpath++)
+	for (fname = buf; (*fname = *fpath) != '\0'; fname++, fpath++)
 		;
 
 	*fname++ = '/';
@@ -196,7 +161,7 @@ static int rm_dir(char *fpath) {
 	readdir(dirp);
 	readdir(dirp);
 
-	while (de = readdir(dirp)) {
+	while ((de = readdir(dirp)) != NULL) {
 		fpath = de->d_name;
 		if (*fpath) {
 			strcpy(fname, fpath);
