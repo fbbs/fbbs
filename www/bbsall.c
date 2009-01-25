8a525c77185efbd603b7524c2fe500d5c08b9cd0
bbshelper <bbshelper@gmail.com>
2009-01-25 13:38:44 +0000

    Use pointer for efficiency.

--- www/bbsall.c
+++ www/bbsall.c
@@ -2,26 +2,25 @@
 
 static int cmpboard(const void *b1, const void *b2)
 {
-	return strcasecmp(((struct boardheader *)b1)->filename,
-			((struct boardheader *)b2)->filename);
+	return strcasecmp((*(struct boardheader **)b1)->filename,
+		(*(struct boardheader **)b2)->filename);
 }
 
 int bbsall_main(void)
 {
-	struct boardheader data[MAXBOARD], *x;
-	int i, total=0;
+	struct boardheader *data[MAXBOARD], *x;
+	int i, total = 0;
 	char *ptr;
 
 	for(i = 0; i < MAXBOARD; i++) {
 		x = &(bcache[i]);
-		if (x->filename[0] <= 32 || x->filename[0]>'z')
+		if (x->filename[0] <= 32 || x->filename[0] > 'z')
 			continue;
 		if (!hasreadperm(&currentuser, x))
 			continue;
-		memcpy(&data[total], x, sizeof(struct boardheader));
-		total++;
+		data[total++] = x;
 	}
-	qsort(data, total, sizeof(struct boardheader), cmpboard);
+	qsort(data, total, sizeof(struct boardheader *), cmpboard);
 
 	printf("<img src=/info/all/banner.jpg align=absmiddle border=0><b> %s [讨论区数: %d]</b>", BBSNAME, total);
 	printf("<center>\n");
@@ -30,31 +29,33 @@ int bbsall_main(void)
 	printf("<tr class=pt9h ><th nowrap>序号</th><th nowrap>讨论区名称</th><th nowrap>类别</th><th nowrap>中文描述</th><th nowrap>版主</th>\n");
 	int cc = 0;
 	for(i = 0; i < total; i++) {
-		int isgroup = (data[i].flag & BOARD_DIR_FLAG) ? 1 : 0;
-		if(specialboard(data[i].filename))
+		int isgroup = (data[i]->flag & BOARD_DIR_FLAG) ? 1 : 0;
+		if(specialboard(data[i]->filename))
 			continue;
 		printf("<tr class=%s valign=top><td align=right>%d</td>",
 				((cc++) % 2) ? "pt9dc" : "pt9lc", i + 1);
 		// Print board name.
         if (isgroup)
             printf("<td nowrap><b><a href=bbsboa?board=%s>[ %s ]</a></b></td>", 
-                   data[i].filename, data[i].filename);
+                   data[i]->filename, data[i]->filename);
         else
             printf("<td nowrap><b><a href=bbsdoc?board=%s>%s</a></b></td>", 
-                   data[i].filename, data[i].filename);
+                   data[i]->filename, data[i]->filename);
 		// Print category.
         if (isgroup)
             printf("<td  nowrap align=center><b>%6.6s</b></td>", "[目录]");
         else
-            printf("<td  nowrap align=center>%6.6s</td>", data[i].title+1);
+            printf("<td  nowrap align=center>%6.6s</td>", data[i]->title + 1);
 		// Print board title.
         printf("<td width=100%%><a href=%s?board=%s><b>%s</b></a><br>", 
-               (isgroup?"bbsboa":"bbsdoc"), data[i].filename, data[i].title+10);
-		showrecommend(data[i].filename, 3, 0);
+               (isgroup?"bbsboa":"bbsdoc"), data[i]->filename, data[i]->title + 10);
+		showrecommend(data[i]->filename, 3, 0);
 		printf("</td>");
 		// Print first BM.
-        ptr = strtok(data[i].BM, " ,;");
-        if (ptr==0)
+		char buf[BM_LEN - 4];
+		memcpy(buf, data[i]->BM, sizeof(buf));
+        ptr = strtok(buf, " ,;");
+        if (ptr == NULL)
 			ptr = (isgroup)? "-" : "诚征版主中";
 		printf("</td><td nowrap><b><a href=bbsqry?userid=%s>%s</a></b></td>", ptr, ptr);
 	}
