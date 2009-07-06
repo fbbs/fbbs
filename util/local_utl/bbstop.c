9dd477fd2270acfa13d61e8180ff299073d42383
bbshelper <bbshelper@gmail.com>
2009-07-06 11:08:41 +0000

    bbstop

--- util/local_utl/bbstop.c
+++ util/local_utl/bbstop.c
@@ -1,227 +1,131 @@
 /* bbstop.c -- compute the top login/stay/post */
-/* $Id: bbstop.c 2 2005-07-14 15:06:08Z root $ */
 
-#include <stdio.h>
-#define REAL_INFO
 #include "bbs.h"
 
-struct userec aman;
-struct userec allman[ MAXUSERS ];
-char   passwd_file[ 256 ];
-char   *home_path;
+enum {
+	KEY_LOGINS = 1,
+	KEY_POSTS = 2,
+	KEY_STAY = 3,
+	KEY_PERF = 4
+};
 
-int login_cmp(b, a)
-struct userec *a, *b;
+int login_cmp(const void *left, const void *right)
 {
-    return (a->numlogins - b->numlogins);
+	const struct userec *a = left;
+	const struct userec *b = right;
+    return (b->numlogins - a->numlogins);
 }
 
-int post_cmp(b, a)
-struct userec *a, *b;
+int post_cmp(const void *left, const void *right)
 {
-    return (a->numposts - b->numposts);
+	const struct userec *a = left;
+	const struct userec *b = right;
+    return (b->numposts - a->numposts);
 }
 
-int stay_cmp(b, a)
-struct userec *a, *b;
+int stay_cmp(const void *left, const void *right)
 {
-    return (a->stay - b->stay);
+	const struct userec *a = left;
+	const struct userec *b = right;
+    return (b->stay - a->stay);
 }
 
-int perm_cmp(b, a)
-struct userec *a, *b;
+int perf_cmp(const void *left, const void *right)
 {
-        return (a->numlogins/3+a->numposts+a->stay/3600)-(b->numlogins/3+b->numposts+b->stay/3600);
+	const struct userec *a = left;
+	const struct userec *b = right;
+    return (b->numlogins / 3 + b->numposts + b->stay / 3600)
+			- (a->numlogins / 3 + a->numposts + a->stay / 3600);
 }
 
-// Add by hightman
-int money_cmp(b, a)
-struct userec *a, *b;
+int getvalue(struct userec *user, int mode)
 {
-    return (a->money+a->nummedals*10000 - b->money - b->nummedals*1000);
-}
-// Add end
-top_login( num )
-{
-    int i, j, rows = (num + 1) / 2;
-    char buf1[ 80 ], buf2[ 80 ];
-
-    printf("\n\n%s", "\
-[1;37m              ===========  [1;36m  ÉÏÕ¾´ÎÊýÅÅÐÐ°ñ [37m   ============ \n\n\
-Ãû´Î ´úºÅ       êÇ³Æ           ´ÎÊý    Ãû´Î ´úºÅ       êÇ³Æ            ´ÎÊý \n\
-==== ===============================   ==== ================================\n[0m\
-");
-    for (i = 0; i < rows; i++) {
-        sprintf(buf1,  "[%2d] %-10.10s %-14.14s %3d",
-            i+1, allman[ i ].userid, allman[ i ].username, 
-            allman[ i ].numlogins );
-        j = i + rows ;
-        sprintf(buf2,  "[%2d] %-10.10s %-14.14s   %3d", 
-            j+1, allman[ j ].userid, allman[ j ].username, 
-            allman[ j ].numlogins );
-
-        printf("%-39.39s%-39.39s[m\n", buf1, buf2); 
-    }
+	switch (mode) {
+		case KEY_LOGINS:
+			return user->numlogins;
+		case KEY_POSTS:
+			return user->numposts;
+		case KEY_STAY:
+			return user->stay / 3600;
+		case KEY_PERF:
+		default:
+			return user->numlogins / 3 + user->numposts + user->stay / 3600;
+	}
+	return 0;
 }
 
-top_stay( num )
+int top_info(const char *item, int num, struct userec *users, int mode)
 {
-    int i, j, rows = (num + 1) / 2;
-    char buf1[ 80 ], buf2[ 80 ];
-
-    printf("\n\n%s", "\
-[1;37m              ===========   [36m ÉÏÕ¾×ÜÊ±ÊýÅÅÐÐ°ñ [37m   ============ \n\n\
-Ãû´Î ´úºÅ       êÇ³Æ           ×ÜÊ±Êý  Ãû´Î ´úºÅ       êÇ³Æ           ×ÜÊ±Êý \n\
-==== ================================  ==== ================================\n[0m\
-");
-    for (i = 0; i < rows; i++) {
-        sprintf(buf1,  "[%2d] %-10.10s %-14.14s%4d:%2d",
-            i+1, allman[ i ].userid, allman[ i ].username, 
-            allman[ i ].stay/3600,(allman[ i ].stay%3600)/60 );
-        j = i + rows ;
-        sprintf(buf2,  "[%2d] %-10.10s %-14.14s%4d:%2d", 
-            j+1, allman[ j ].userid, allman[ j ].username, 
-             allman[ j ].stay/3600,(allman[ j ].stay%3600)/60 );
-
-        printf("%-39.39s%-39.39s[m\n", buf1, buf2); 
-    }
+	printf("Ãû´Î ÕÊºÅ         %6.6s Ãû´Î ÕÊºÅ         %6.6s Ãû´Î ÕÊºÅ         %6.6s\n"
+			"==== ============ ====== ==== ============ ====== ==== ============ ======\n",
+			item, item, item);
+	int rows = (num + 2) / 3;
+	int i, j, rank;
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < 3; j++) {
+			rank = i + j * rows;
+			if (rank < num)
+				printf("[%2d] %-12.12s %6d ", rank + 1, users[rank].userid, 
+						getvalue(users + rank, mode));
+		}
+		printf("\n");
+	}
 }
 
-top_post( num )
-{
-    int i, j, rows = (num + 1) / 2;
-    char buf1[ 80 ], buf2[ 80 ];
-
-    printf("\n\n%s", "\
-              [1;37m===========  [36m  ÌÖÂÛ´ÎÊýÅÅÐÐ°ñ [37m   ============ \n\n\
-Ãû´Î ´úºÅ       êÇ³Æ           ´ÎÊý    Ãû´Î ´úºÅ       êÇ³Æ            ´ÎÊý \n\
-==== ===============================  ===== ================================\n[0m\
-");
-    for (i = 0; i < rows; i++) {
-        sprintf(buf1,  "[%2d] %-10.10s %-14.14s %3d",
-            i+1, allman[ i ].userid, allman[ i ].username, 
-            allman[ i ].numposts );
-        j = i + rows;
-        sprintf(buf2,  "[%2d] %-10.10s %-14.14s   %3d", 
-            j+1, allman[ j ].userid, allman[ j ].username, 
-            allman[ j ].numposts );
-
-        printf("%-39.39s%-39.39s[m\n", buf1, buf2);
-    }
-}
-
-top_perm( num )
-{
-    int i, j, rows = (num + 1) / 2;
-    char buf1[ 80 ], buf2[ 80 ];
-
-    printf("\n\n%s", "\
-              [1;37m===========    [36m×Ü±íÏÖ»ý·ÖÅÅÐÐ°ñ[37m    ============ \n\
-                   [32m ¹«Ê½£ºÉÏÕ¾´ÎÊý/3+ÎÄÕÂÊý+ÉÏÕ¾¼¸Ð¡Ê±[37m\n\
-Ãû´Î ´úºÅ       êÇ³Æ            »ý·Ö   Ãû´Î ´úºÅ       êÇ³Æ              »ý·Ö \n\
-==== ===============================   ==== =================================\n[0m\
-");
-    for (i = 0; i < rows; i++) {
-        sprintf(buf1,  "[%2d] %-10.10s %-14.14s %5d",
-            i+1, allman[ i ].userid, allman[ i ].username, 
-            (allman[ i ].numlogins/3)+allman[ i ].numposts+(allman[ i ].stay/3600));
-        j = i + rows;
-        sprintf(buf2,  "[%2d] %-10.10s %-14.14s   %5d", 
-            j+1, allman[ j ].userid, allman[ j ].username, 
-            (allman[ j ].numlogins/3)+allman[ j ].numposts+(allman[ j ].stay/3600) );
-
-        printf("%-39.39s%-39.39s[m\n", buf1, buf2);
-    }
-}
-//add by hightman
-top_money( num )
-{
-    int i, j, rows = (num + 1) / 2;
-    char buf1[ 80 ], buf2[ 80 ];
-
-    printf("\n\n%s", "\
-              [1;37m===========  [36m  ½ñÈÕ´æ¿îÅÅÐÐ°ñ [37m   ============\n"
-"\n\nÃû´Î ´úºÅ       êÇ³Æ           Ç®Êý    Ãû´Î ´úºÅ       êÇ³Æ            Ç®Êý \n\
-==== ================================= ===== ===============================[0m\n");
-    for (i = 0; i < rows; i++) {
-        sprintf(buf1,  "[%2d] %-10.10s %-12.12s %8d",
-            i+1, allman[ i ].userid, allman[ i ].username,
-            allman[ i ].money+allman[i].nummedals*10000);
-        j = i + rows;
-        sprintf(buf2,  "[%2d] %-10.10s %-12.12s %6d",
-            j+1, allman[ j ].userid, allman[ j ].username,
-            allman[ j ].money+allman[j].nummedals*10000);
-
-        printf("%-39.39s%-39.39s[m\n", buf1, buf2);
-    }
-}
-//add end
-
-main(argc, argv)
-int  argc;
-char **argv;
+int main(int argc, char **argv)
 {
     FILE *inf;
-    int  i, no = 0,mode=0;
+    int  i, num = 0, mode = 0;
+	char passwd_file[256];
+	char *home_path;
+	struct userec alluser[MAXUSERS];
+	struct userec *user = alluser;
+	struct userec *end = user + MAXUSERS;
 
     if (argc < 4 ) {
-        printf("Usage: %s bbs_home num_top mode\nmode=(0All 1Logins 2Posts 3Stay 4perm 5money)\n", argv[ 0 ]);
-        exit( 1 );
+        printf("Usage: %s bbs_home num_top mode\nmode=(0All 1Logins 2Posts 3Stay 4Perf)\n", argv[0]);
+        exit(1);
     }
-
-    home_path = argv[ 1 ];
+    home_path = argv[1];
     sprintf(passwd_file, "%s/.PASSWDS", home_path);
     
-    no = atoi( argv[ 2 ] );
-    mode=atoi(argv[ 3 ]);
-    if(mode>5||mode<1)
-        mode=0;
-    if (no == 0) no = 20;
+    num = atoi(argv[2]);
+    mode = atoi(argv[3]);
+    if (mode > KEY_PERF || mode < KEY_LOGINS)
+        mode = 0;
+    if (num == 0)
+		num = 20;
 
-    inf = fopen( passwd_file, "rb" );
-
+    inf = fopen(passwd_file, "rb");
     if (inf == NULL) { 
         printf("Sorry, the data is not ready.\n"); 
-        exit( 0 ); 
-    }
+        exit(0);
+	}
 
-    for (i=0; ; i++) {
-        if (fread(&allman[ i ], sizeof( aman ), 1, inf ) <= 0) break;
-        if(strcmp(allman[ i ].userid,"guest")==0)
-        {
-                i--;
-                continue;
-        }
-    }
+	for (user = alluser; user < end; user++) {
+		if (fread(user, sizeof(*user), 1, inf) <= 0)
+			break;
+		if (!strcmp(user->userid, "guest")) {
+			user--;
+			continue;
+		}
+	}
 
-    if(mode==1||mode==0)
-    {
-            qsort(allman, i, sizeof( aman ), login_cmp);
-            top_login( no );
+	if (mode == KEY_LOGINS || mode == 0) {
+		qsort(alluser, sizeof(alluser) / sizeof(*user), sizeof(*user), login_cmp);
+		top_info("ÉÏÕ¾Êý", num, alluser, KEY_LOGINS);
     }
-
-    if(mode==2||mode==0)
-    {
-            qsort(allman, i, sizeof( aman ), post_cmp);
-            top_post( no );
+	if (mode == KEY_POSTS || mode == 0) {
+		qsort(alluser, sizeof(alluser) / sizeof(*user), sizeof(*user), post_cmp);
+		top_info("·¢ÎÄÊý", num, alluser, KEY_POSTS);
     }
-
-    if(mode==3||mode==0)
-    {
-            qsort(allman, i, sizeof( aman ), stay_cmp);
-            top_stay( no );
+	if (mode == KEY_STAY || mode == 0) {
+		qsort(alluser, sizeof(alluser) / sizeof(*user), sizeof(*user), stay_cmp);
+		top_info("×ÜÊ±Êý", num, alluser, KEY_STAY);
     }
-    if(mode==4||mode==0)
-    {
-            qsort(allman, i, sizeof( aman ), perm_cmp);
-            top_perm( no );
+	if (mode == KEY_PERF || mode == 0) {
+		qsort(alluser, sizeof(alluser) / sizeof(*user), sizeof(*user), perf_cmp);
+		top_info("×Ü»ý·Ö", num, alluser, KEY_PERF);
     }
-// Add by hightman
-    if(mode==5||mode==0)
-    {
-           qsort(allman, i, sizeof( aman ), money_cmp);
-           top_money( no );
-    }
-// Add end
-    printf("\n");
+	return 0;
 }
