93aaf4136b111e25cd85d2198cc099ab1cf98c6e
bbshelper <bbshelper@gmail.com>
2009-07-23 14:39:03 +0000

    use mmap in bbsdoc. extracted 2 methods.

--- www/bbsdoc.c
+++ www/bbsdoc.c
@@ -1,5 +1,74 @@
 #include "libweb.h"
 
+bool allow_reply(struct fileheader *fh)
+{
+	if (fh == NULL || fh->accessed[0] & FILE_NOREPLY)
+		return false;
+	return true;
+}
+
+int get_post_mark(struct fileheader *fh, bool has_read)
+{
+	int mark = ' ';
+	if (fh == NULL)
+		return mark;
+	if (fh->accessed[0] & FILE_DIGEST)
+		mark = 'g';
+	if (fh->accessed[0] & FILE_MARKED) {
+		if (mark == ' ')
+			mark = 'm';
+		else
+			mark = 'b';
+	}
+	if (fh->accessed[0] & FILE_DELETED && mark == ' ')
+		mark = 'w';
+	if (!has_read) {
+		if (mark == ' ')
+			mark = '+';
+		else
+			mark = toupper(mark);
+	}
+	return mark;
+}
+
+// Read 'count' files starting from 'start'(origin 1) in index 'dir' and 
+// prints file information in XML format.
+// Returns files in total on success, -1 on error.
+int bbsdoc(const char *dir, int *start, int count)
+{
+	void *ptr;
+	size_t size;
+	int fd, total, mark;
+	if (safe_mmapfile(dir, O_RDONLY, PROT_READ, MAP_SHARED, &ptr, &size, &fd)) {
+		total = size / sizeof(struct fileheader);
+		if (*start <= 0 || *start > total - count)
+			*start = total - count + 1;
+		if (*start < 1) {
+			*start = 1;
+			count = total;
+		}
+		struct fileheader *fh = (struct fileheader *)ptr + *start - 1;
+		struct fileheader *end = fh + count;
+		for (; fh < end; ++fh) {
+			printf("<post>\n<author>%s</author>\n<time>%s</time>\n"
+					"<id>%u</id>\n<title>",
+					fh->owner, getdatestring(getfiletime(fh), DATE_XML), fh->id);
+			xml_fputs(fh->title, stdout);
+			printf("</title>\n");
+			if (!allow_reply(fh))
+				printf("<noreply />\n");
+			mark = get_post_mark(fh, brc_unread(fh->filename));
+			if (mark != ' ')
+				printf("<mark>%c</mark></post>\n", mark);
+			else
+				printf("</post>\n");
+     	}
+		end_mmapfile(ptr, size, fd);
+		return total;
+	}
+	return -1;
+}
+
 int bbsdoc_main(void)
 {
 	char board[STRLEN];
@@ -20,16 +89,11 @@ int bbsdoc_main(void)
 
 	char dir[HOMELEN];
 	setbfile(dir, board, DOT_DIR);
-	int total = file_size(dir) / sizeof(struct fileheader);
 	int start = strtol(getparm("start"), NULL, 10);
 	int my_t_lines = strtol(getparm("my_t_lines"), NULL, 10);
 	int bid = getbnum2(bp);
 	if (my_t_lines < 10 || my_t_lines > 40)
 		my_t_lines = TLINES;
-	if (strlen(getparm("start")) == 0 || start > total - my_t_lines)
-		start = total - my_t_lines + 1;
-  	if (start < 1)
-		start = 1;
 	brc_initial(currentuser.userid, board);
 
 	xml_header("bbsdoc");
@@ -41,54 +105,12 @@ int bbsdoc_main(void)
 	sprintf(path, "%s/info/boards/%s/banner.jpg", BBSHOME, board);
 	if(dashf(path))	
 		printf("<banner>%s</banner>\n", path);
-
+	int total = bbsdoc(dir, &start, my_t_lines);
 	// TODO: magic number.
 	printf("<title>%s</title>\n<bm>%s</bm>\n<desc>%s</desc>\n"
 			"<total>%d</total>\n<start>%d</start>\n<bid>%d</bid>\n"
-			"<page>%d</page>\n", bp->filename, bp->BM, bp->title + 11, total,
-			start, bid, my_t_lines);
-
+			"<page>%d</page></bbsdoc>", bp->filename, bp->BM, bp->title + 11,
+			total, start, bid, my_t_lines);
 	// TODO: marquee, recommend, spin
-	FILE *fp = fopen(dir, "r");
-	if (fp != NULL) {
-		fseek(fp, (start - 1) * sizeof(struct fileheader), SEEK_SET);
-		struct fileheader x;
-		int type;
-		for (int i = 0; i < my_t_lines; i++) {
-			type = ' ';
-			if (fread(&x, sizeof(x), 1, fp) <= 0)
-				break;
-			printf("<post>\n<author>%s</author>\n<time>%s</time>\n"
-					"<id>%u</id>\n<title>",
-					x.owner, getdatestring(getfiletime(&x), DATE_XML), x.id);
-			xml_fputs(x.title, stdout);
-			printf("</title>\n");
-			if (x.accessed[0] & FILE_NOREPLY)
-				printf("<noreply />\n");
-			// Mark
-			if (x.accessed[0] & FILE_DIGEST)
-				type = 'g';
-			if (x.accessed[0] & FILE_MARKED) {
-				if (type == ' ')
-					type = 'm';
-				else
-					type = 'b';
-			}
-			if (x.accessed[0] & FILE_DELETED && type == ' ')
-				type = 'w';
-			if (brc_unread(x.filename)) {
-				if (type == ' ')
-					type = '+';
-				else
-					type = toupper(type);
-			}
-			if (type != ' ')
-				printf("<mark>%c</mark></post>\n", type);
-			else
-				printf("</post>\n");
-     	}
-		fclose(fp);
-	}
-	printf("</bbsdoc>");
 	return 0;
 }
