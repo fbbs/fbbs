f264ceacbf0a69a375ebf67de2232f301f5f1bb1
bbshelper <bbshelper@gmail.com>
2010-04-01 02:58:22 +0000

    Merged bbsfwd and bbsccc to bbspst.c.

--- www/bbspst.c
+++ www/bbspst.c
@@ -132,3 +132,108 @@ int bbsedit_main(void)
 {
 	return do_bbspst(true);
 }
+
+int post_article(const struct userec *user, const struct boardheader *bp,
+		const char *title, const char *content, int sig, bool cross,
+		const char *ip, const struct fileheader *o_fp);
+
+int bbsccc_main(void)
+{
+	if (!loginok)
+		return BBS_ELGNREQ;
+	int bid = strtol(getparm("bid"), NULL, 10);
+	struct boardheader *bp = getbcache2(bid);
+	if (bp == NULL || !hasreadperm(&currentuser, bp))
+		return BBS_ENOBRD;
+	if (bp->flag & BOARD_DIR_FLAG)
+		return BBS_EINVAL;
+
+	unsigned int fid = strtoul(getparm("f"), NULL, 10);
+	struct fileheader fh;
+	if (!bbscon_search(bp, fid, 0, &fh))
+		return BBS_ENOFILE;
+
+	char *target = getparm("t");
+	if (*target != '\0') {
+		struct boardheader *bp2 = getbcache(target);
+		if (bp2 == NULL)
+			return BBS_ENOBRD;
+		if (bp2->flag & BOARD_DIR_FLAG || bp2 == bp)
+			return BBS_EINVAL;
+		if (!haspostperm(&currentuser, bp2))
+			return BBS_EPST;
+		mmap_t m;
+		m.oflag = O_RDONLY;
+		char file[HOMELEN];
+		setbfile(file, bp->filename, fh.filename);
+
+		char title[sizeof(fh.title)];
+		if (strncmp(fh.title, "[转载]", sizeof("[转载]") - 1) == 0)
+			strlcpy(title, fh.title, sizeof(title));
+		else
+			snprintf(title, sizeof(title), "[转载]%s", fh.title);
+		if (mmap_open(file, &m) < 0)
+			return BBS_EINTNL;
+		int ret = post_article(&currentuser, bp2, title, m.ptr, 0, true,
+				fromhost, NULL);
+		mmap_close(&m);
+		if (ret < 0)
+			return BBS_EINTNL;
+		xml_header("bbs");
+		printf("<bbsccc t='%d' b='%d' ", bp2 - bcache + 1, bp - bcache + 1);
+		print_session();
+		printf("/>");
+	} else {
+		xml_header("bbs");
+		printf("<bbsccc owner='%s' brd='%s' bid='%d' fid='%u' ", 
+				fh.owner, bp->filename, bp - bcache + 1, fid);
+		print_session();
+		printf(">");
+		xml_fputs(fh.title, stdout);
+		printf("</bbsccc>");
+	}
+	return 0;
+}
+
+/**
+ * Forward post.
+ * @return 0 on success, bbserrno on error.
+ */
+// fwd?bid=[bid]&f=[fid]&u=[recipient]
+int bbsfwd_main(void)
+{
+	if (!loginok)
+		return BBS_ELGNREQ;
+	parse_post_data();
+	char *reci = getparm("u");
+	if (*reci == '\0') {
+		xml_header("bbs");
+		printf("<bbsfwd bid='%s' f='%s' ", getparm("bid"), getparm("f"));
+		print_session();
+		printf("></bbsfwd>");
+	} else {
+		if (!HAS_PERM(PERM_MAIL))
+			return BBS_EACCES;
+		int bid = strtol(getparm("bid"), NULL, 10);
+		struct boardheader *bp = getbcache2(bid);
+		if (bp == NULL || !hasreadperm(&currentuser, bp))
+			return BBS_ENOBRD;
+		if (bp->flag & BOARD_DIR_FLAG)
+			return BBS_EINVAL;
+		unsigned int fid = strtoul(getparm("f"), NULL, 10);
+		struct fileheader fh;
+		if (!bbscon_search(bp, fid, 0, &fh))
+			return BBS_ENOFILE;
+		char file[HOMELEN];
+		setbfile(file, bp->filename, fh.filename);
+		char title[STRLEN];
+		snprintf(title, sizeof(title), "[转寄]%s", fh.title);
+		int ret = mail_file(file, reci, title);
+		if (ret)
+			return ret;
+		http_header();
+		printf("</head><body><p>文章转寄成功</p>"
+				"<a href='javascript:history.go(-2)'>返回</a></body></html>");
+	}
+	return 0;
+}
