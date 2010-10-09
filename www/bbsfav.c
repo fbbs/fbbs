3c3d6f0cdf81a7dce8be5d8db63db84a78b6626e
bbshelper <bbshelper@gmail.com>
2010-10-09 17:46:46 +0800

    do not return error when fav record doesn't exist

--- www/bbsfav.c
+++ www/bbsfav.c
@@ -7,32 +7,30 @@ int bbsfav_main(void)
 	if (!loginok)
 		return BBS_ELGNREQ;
 
-	// Read '.goodbrd'
+	xml_header(NULL);
+	printf("<bbsfav>");
+	print_session();
+
 	char file[HOMELEN];
 	sethomefile(file, currentuser.userid, ".goodbrd");
 	mmap_t m;
 	m.oflag = O_RDONLY;
-	if (mmap_open(file, &m) < 0)
-		return BBS_ENOFILE;
-	struct goodbrdheader *iter, *end;
-	int num = m.size / sizeof(struct goodbrdheader);
-	if (num > GOOD_BRC_NUM)
-		num = GOOD_BRC_NUM;
-	end = (struct goodbrdheader *)m.ptr + num;
-
-	// Print all favorite boards.
-	xml_header(NULL);
-	printf("<bbsfav>");
-	print_session();
-	for (iter = m.ptr; iter != end; ++iter) {
-		if (!gbrd_is_custom_dir(iter)) {
-			struct boardheader *bp = bcache + iter->pos;
-			printf("<brd bid='%d' brd='%s'>", iter->pos + 1, bp->filename);
-			xml_fputs(get_board_desc(bp), stdout);
-			printf("</brd>");
+	if (mmap_open(file, &m) == 0) {
+		struct goodbrdheader *iter, *end;
+		int num = m.size / sizeof(struct goodbrdheader);
+		if (num > GOOD_BRC_NUM)
+			num = GOOD_BRC_NUM;
+		end = (struct goodbrdheader *)m.ptr + num;
+		for (iter = m.ptr; iter != end; ++iter) {
+			if (!gbrd_is_custom_dir(iter)) {
+				struct boardheader *bp = bcache + iter->pos;
+				printf("<brd bid='%d' brd='%s'>", iter->pos + 1, bp->filename);
+				xml_fputs(get_board_desc(bp), stdout);
+				printf("</brd>");
+			}
 		}
+		mmap_close(&m);
 	}
-	mmap_close(&m);
 	printf("</bbsfav>");
 	return 0;
 }
