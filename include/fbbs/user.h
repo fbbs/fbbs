8070e9dad4ccaa81507b3bd3bfdd2662bd364fd6
bbshelper <bbshelper@gmail.com>
2011-04-22 21:18:18 +0800

    add user.h

---
+++ include/fbbs/user.h
@@ -0,0 +1,17 @@
+#ifndef FB_USER_H
+#define FB_USER_H
+
+#include "fbbs/dbi.h"
+
+enum {
+	USER_FIELD_LOGINS = 0,
+	USER_FIELD_POSTS,
+	USER_FIELD_STAY,
+};
+
+extern int get_user_id(db_conn_t *c, const char *name);
+extern int user_data_add_by_name(db_conn_t *c, const char *name,
+		int field, int delta);
+extern int user_data_add(db_conn_t *c, int uid, int field, int delta);
+
+#endif // FB_USER_H
