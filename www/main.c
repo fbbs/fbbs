118ca72972a4de48c810df01a37697b8ab1bf615
bbshelper <bbshelper@gmail.com>
2011-06-18 19:49:34 +0800

    update _init_all() and some cleanup

--- www/main.c
+++ www/main.c
@@ -1,13 +1,15 @@
 #include "libweb.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include "fbbs/fbbs.h"
+#include "fbbs/string.h"
 #include "fbbs/web.h"
 
 void check_bbserr(int err);
 extern int bbssec_main(web_ctx_t *ctx);
 extern int bbsall_main(web_ctx_t *ctx);
 extern int bbsboa_main(web_ctx_t *ctx);
-extern int bbslogin_main(web_ctx_t *ctx);
+extern int web_login(web_ctx_t *ctx);
 extern int bbslogout_main(web_ctx_t *ctx);
 extern int bbsdoc_main(web_ctx_t *ctx);
 extern int bbscon_main(web_ctx_t *ctx);
@@ -63,11 +65,11 @@ typedef struct {
 	int mode;            ///< user mode. @see mode_type
 } web_handler_t;
 
-const static web_handler_t applets[] = {
+const static web_handler_t handlers[] = {
 		{"sec", bbssec_main, READBRD},
 		{"all", bbsall_main, READBRD},
 		{"boa", bbsboa_main, READNEW},
-		{"login", bbslogin_main, LOGIN},
+		{"login", web_login, LOGIN},
 		{"logout", bbslogout_main, MMENU},
 		{"doc", bbsdoc_main, READING},
 		{"con", bbscon_main, READING},
@@ -119,48 +121,67 @@ const static web_handler_t applets[] = {
 		{NULL, NULL, 0}
 };
 
+bbs_env_t env;
+
 /**
  * Get an web request handler according to its name.
  * @return handler pointer if found, NULL otherwise.
  */
-static const web_handler_t *getapplet(void)
+static const web_handler_t *_get_handler(void)
 {
 	char *surl = getenv("SCRIPT_NAME");
-	if (surl == NULL)
+	if (!surl)
 		return NULL;
-	char *result = strrchr(surl, '/');
-	if (result == NULL)
-		result = surl;
+
+	char *name = strrchr(surl, '/');
+	if (!name)
+		name = surl;
 	else
-		result++;	
-	const web_handler_t *app = applets;
-	while (app->name != NULL) {
-		if (!strcmp(result, app->name))
-			return app;
-		app++;
+		++name;
+
+	const web_handler_t *h = handlers;
+	while (h->name) {
+		if (streq(name, h->name))
+			return h;
+		++h;
 	}
 	return NULL;
 }
 
 /**
- * Initialization before any FastCGI loop.
- * @return 0 on success, BBS_EINTNL on error.
+ * Initialization before entering FastCGI loop.
+ * @return 0 on success, -1 on error.
  */
-static int fcgi_init_all(void)
+static int _init_all(void)
 {
 	srand(time(NULL) * 2 + getpid());
+
 	if (chdir(BBSHOME) != 0)
-		return BBS_EINTNL;
+		return -1;
+
 	seteuid(BBSUID);
 	if(geteuid() != BBSUID)
-		return BBS_EINTNL;
-	if (resolve_ucache() == -1)
-		return BBS_EINTNL;
-	resolve_utmp();
+		return -1;
+
 	if (resolve_boards() < 0)
-		return BBS_EINTNL;
-	if (utmpshm == NULL || brdshm == NULL)
-		return BBS_EINTNL;
+		return -1;
+	if (!brdshm)
+		return -1;
+
+	env.p = pool_create(DEFAULT_POOL_SIZE);
+	if (!env.p)
+		return -1;
+
+	env.c = config_load(env.p, DEFAULT_CFG_FILE);
+	if (!env.c)
+		return -1;
+
+	env.d = db_connect(config_get(env.c, "host"), config_get(env.c, "port"),
+			config_get(env.c, "dbname"), config_get(env.c, "user"),
+			config_get(env.c, "password"));
+	if (db_status(env.d) != DB_CONNECTION_OK)
+		return -1;
+
 	return 0;
 }
 
@@ -170,7 +191,7 @@ static int fcgi_init_all(void)
  */
 int main(void)
 {
-	if (fcgi_init_all() < 0)
+	if (_init_all() < 0)
 		return EXIT_FAILURE;
 
 	convert_t u2g, g2u;
@@ -184,20 +205,20 @@ int main(void)
 		if (!ctx.r)
 			return EXIT_FAILURE;
 
-		const web_handler_t *app = getapplet();
+		const web_handler_t *h = _get_handler();
 		int ret;
-		if (app == NULL) {
+		if (!h) {
 			ret = BBS_ENOURL;
 		} else {
-			fcgi_init_loop(&ctx, get_web_mode(app->mode));
+			fcgi_init_loop(&ctx, get_web_mode(h->mode));
 #ifdef FDQUAN
-			if (!loginok && app->func != bbslogin_main
-					&& app->func != fcgi_reg && app->func != fcgi_activate)
+			if (!loginok && h->func != web_login && h->func != fcgi_reg
+					&& h->func != fcgi_activate)
 				ret = BBS_ELGNREQ;
 			else
-				ret = (*(app->func))(&ctx);
+				ret = (*(h->func))(&ctx);
 #else
-			ret = (*(app->func))(&ctx);
+			ret = (*(h->func))(&ctx);
 #endif // FDQUAN
 		}
 		check_bbserr(ret);
