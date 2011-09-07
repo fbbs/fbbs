ed1859c461fa6af73d0b62f5d97cff8632e16fea
bbshelper <bbshelper@gmail.com>
2011-09-07 22:02:49 +0800

    new db schema

--- pg/schema.sql
+++ pg/schema.sql
@@ -1,96 +1,12 @@
 BEGIN;
 
-CREATE TABLE emails (
-	id SERIAL PRIMARY KEY,
-	addr TEXT
-);
-
-CREATE TABLE users (
+CREATE TABLE all_users (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
-	passwd TEXT,
-	nick TEXT,
-	email INTEGER,
-	flags BIGINT,
-	perm INTEGER,
-	logins INTEGER DEFAULT 0,
-	posts INTEGER DEFAULT 0,
-	stay INTEGER DEFAULT 0,
-	medals INTEGER DEFAULT 0,
-	money INTEGER DEFAULT 0,
-	birth TIMESTAMPTZ,
-	gender CHAR,
-	creation TIMESTAMPTZ,
-	lastlogin TIMESTAMPTZ,
-	lastlogout TIMESTAMPTZ,
-	lasthost TEXT
+	passwd TEXT
 );
+
+CREATE TABLE users () INHERITS (all_users);
 CREATE UNIQUE INDEX user_name_idx ON users (lower(name));
-
-CREATE TABLE groups (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	description TEXT
-);
-
-CREATE TABLE roles (
-	user_id INTEGER,
-	group_id INTEGER,
-	grant_type INTEGER
-);
-CREATE INDEX roles_user_id_idx ON roles (user_id);
--- unique index?
-
-CREATE TABLE sectors (
-	id SERIAL PRIMARY KEY,
-	name TEXT
-);
-
-CREATE TABLE boards (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	description TEXT,
-	category TEXT,
-	sector INTEGER,
-	parent INTEGER,
-	property INTEGER
-);
-
-CREATE TABLE managers (
-	user_id INTEGER,
-	board_id INTEGER,
-	appointed TIMESTAMPTZ
-);
-
-CREATE TABLE posts (
-	id BIGSERIAL PRIMARY KEY,
-	reid BIGINT,
-	gid BIGINT,
-	board_id INTEGER,
-	user_id INTEGER,
-	user_name TEXT, -- for compatability
-	title TEXT,
-	property INTEGER, --?
-	filename TEXT,
-	time TIMESTAMPTZ
-);
-
-CREATE TABLE sessions (
-	id BIGSERIAL PRIMARY KEY,
-	user_id INTEGER,
-	key BIGINTEGER,
-	pid INTEGER,
-	property INTEGER,
-        -- bits: invisible, sockactive, pager, in_chat
-	status INTEGER, --(mode)
-	board_id INTEGER, --(currbrdnum)
-	fromhost TEXT,
-	chat_id TEXT,
-	start_time TIMESTAMPTZ,
-	expire_time TIMESTAMPTZ,
-	idle_time TIMESTAMPTZ
-	-- sockaddr(sim_port), destuid, last_post_time
-);
-
 
 COMMIT;
