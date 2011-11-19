3277815891dce8a825cdc995a3570f00fda25283
bbshelper <bbshelper@gmail.com>
2011-11-19 21:53:14 +0800

    fix bug in 424a80eb8812

--- util/local_utl/cleanuser.c
+++ util/local_utl/cleanuser.c
@@ -170,7 +170,7 @@ int main(int argc, char **argv)
 			// for now, we just delete them one by one.
 			db_res_t *res = db_exec_cmd(_env.db,
 					"UPDATE all_users SET alive = FALSE"
-					" WHERE lower(name) = lower(%s) AND alive = FALSE",
+					" WHERE lower(name) = lower(%s) AND alive = TRUE",
 					user.userid);
 			db_clear(res);
 		}
