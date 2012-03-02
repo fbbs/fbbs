4ef3fa46d41c0a32f70517ff40c6ef7afdddbdf6
bbshelper <bbshelper@gmail.com>
2012-03-02 20:56:33 +0800

    generate_session_key

--- include/fbbs/session.h
+++ include/fbbs/session.h
@@ -15,7 +15,6 @@ enum {
 };
 
 extern session_id_t session_new_id(void);
-extern char *session_generate_key(char *buf, size_t size);
 extern session_id_t session_new(const char *key, session_id_t sid, user_id_t uid,
 		const char *ip_addr);
 
--- lib/session.c
+++ lib/session.c
@@ -28,12 +28,6 @@ session_id_t session_new_id(void)
 	return sid;
 }
 
-// TODO
-char *session_generate_key(char *buf, size_t size)
-{
-	return buf;
-}
-
 session_id_t session_new(const char *key, session_id_t sid, user_id_t uid,
 		const char *ip_addr)
 {
