6ec52718ee6470ed3169e6f7c9611a0a41cc41e8
bbshelper <bbshelper@gmail.com>
2012-08-11 13:02:11 +0800

    normalize http status code enums

--- include/fbbs/web.h
+++ include/fbbs/web.h
@@ -22,6 +22,16 @@ enum {
 	REQUEST_UTF8 = 0x8,
 };
 
+enum {
+	HTTP_OK = 200,
+	HTTP_BAD_REQUEST = 400,
+	HTTP_UNAUTHORIZED = 401,
+	HTTP_FORBIDDEN = 403,
+	HTTP_NOT_FOUND = 404,
+	HTTP_INTERNAL_SERVER_ERROR = 500,
+	HTTP_SERVICE_UNAVAILABLE = 503,
+};
+
 typedef struct pair_t {
 	char *key;
 	char *val;
