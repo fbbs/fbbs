23e54b1f6e32152839edb4f921232796ee188078
bbshelper <bbshelper@gmail.com>
2012-08-19 21:31:59 +0800

    simple post_list_display

--- src/plist.c
+++ src/plist.c
@@ -235,8 +235,30 @@ static slide_list_title_t post_list_title(slide_list_t *p)
 	return;
 }
 
+static void post_list_display_entry(post_info_t *p)
+{
+	GBK_BUFFER(title, POST_TITLE_CCHARS);
+	convert_u2g(p->utf8_title, gbk_title);
+	printf("%s %s\n", p->owner, p->gbk_title);
+}
+
 static slide_list_display_t post_list_display(slide_list_t *p)
 {
+	post_list_t *l = p->data;
+
+	if (!l->posts)
+		return;
+	for (int i = 0; i < l->posts; ++i) {
+		post_list_display_entry(l->posts + i);
+	}
+
+	if (!l->sposts)
+		return;
+	int remain = t_lines - 4 - l->posts;
+	for (int i = 0; i < remain && i < l->scount; ++i) {
+		post_list_display_entry(l->sposts + i);
+	}
+
 	return 0;
 }
 
