cd0f782ab34f4479fc426e2fd4e88750a1f370ef
bbshelper <bbshelper@gmail.com>
2012-09-15 15:41:07 +0800

    add copyright notice

--- lib/pool.c
+++ lib/pool.c
@@ -1,9 +1,41 @@
+/*
+ * Code taken from nginx src/core/ngx_palloc.c with modifications.
+ *
+ * Copyright (C) 2002-2010 Igor Sysoev
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "fbbs/pool.h"
 #include "fbbs/util.h"
 #include <string.h>
+
+/**
+ * @defgroup pool Memory pool
+ * @{
+ */
 
 enum {
 	ALIGNMENT = sizeof(unsigned long),
@@ -197,3 +229,5 @@ char *pool_strdup(pool_t *p, const char *str, size_t size)
 		memcpy(dst, str, size);
 	return dst;
 }
+
+/** @} */
