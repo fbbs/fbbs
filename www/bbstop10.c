f670a64631cb584782d72583fe1e1fec3e62af7e
bbshelper <bbshelper@gmail.com>
2012-11-21 21:34:58 +0800

    fix web top10

--- www/bbstop10.c
+++ www/bbstop10.c
@@ -1,37 +1,37 @@
 #include "libweb.h"
+#include "fbbs/dbi.h"
+#include "fbbs/post.h"
 #include "fbbs/string.h"
 #include "fbbs/web.h"
-
-enum {
-    BOARD_LEN = 18, TITLE_LEN = 62, OWNER_LEN = 16,
-};
-
-typedef struct top_t {
-    unsigned int gid;
-    char board[BOARD_LEN];
-    char title[TITLE_LEN];
-    char owner[OWNER_LEN];
-    int count;
-    time_t last;
-} top_t;
 
 int bbstop10_main(void)
 {
 	xml_header(NULL);
 	printf("<bbstop10>");
 	print_session();
-	top_t top;
-	FILE *fp = fopen(BBSHOME"/etc/posts/day_f.data", "rb");
-	if (fp != NULL) {
-		for (int i = 0; i < 10; ++i) {
-			if (fread(&top, sizeof(top), 1, fp) != 1)
-				break;
-			printf("<top board='%s' owner='%s' count='%d' gid='%u'>",
-					top.board, top.owner, top.count, top.gid);
-			xml_fputs2(top.title, check_gbk(top.title) - top.title, stdout);
-			printf("</top>\n");
-		}
-		fclose(fp);
+
+	query_t *q = query_new(0);
+	query_select(q, "tid, score, uname, bname, title");
+	query_from(q, "posts.hot");
+	query_orderby(q, "score", false);
+	query_limit(q, 10);
+
+	db_res_t *res = query_exec(q);
+	int rows = db_res_rows(res);
+	for (int i = 0; i < rows; ++i) {
+		post_id_t tid = db_get_post_id(res, i, 0);
+		int score = db_get_integer(res, i, 1);
+		const char *uname = db_get_value(res, i, 2);
+		const char *bname = db_get_value(res, i, 3);
+		const char *title = db_get_value(res, i, 4);
+
+		GBK_BUFFER(title, POST_TITLE_CCHARS);
+		convert_u2g(title, gbk_title);
+
+		printf("<top board='%s' owner='%s' count='%d' gid='%"PRIdPID"'>",
+				bname, uname, score, tid);
+		xml_fputs2(gbk_title, check_gbk(gbk_title) - gbk_title, stdout);
+		printf("</top>\n");
 	}
 	printf("</bbstop10>");
 	return 0;
