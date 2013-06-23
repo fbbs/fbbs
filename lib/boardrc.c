d56d2aa8c80d853e1ecc03bbcda5fdd8cd2fae90
bbshelper <bbshelper@gmail.com>
2013-06-23 11:59:59 +0800

    brc.num => brc.size, int => uint

--- lib/boardrc.c
+++ lib/boardrc.c
@@ -15,29 +15,31 @@ enum {
 };
 
 typedef uint32_t brc_item_t;
+typedef uint_t brc_size_t;
 
 static struct {
-	int size;
+	brc_size_t size;
 	char ptr[BRC_MAXSIZE];
 } brc_buf;
 
 static struct {
-	int num;
+	brc_size_t size;
 	bool changed;
 	char name[BRC_STRLEN];
 	brc_item_t list[BRC_MAXNUM];
 } brc;
 
-static char *brc_get_record(char *ptr, char *name, int *pnum, brc_item_t *list)
+static char *brc_get_record(char *ptr, char *name, brc_size_t *psize,
+		brc_item_t *list)
 {
 	strlcpy(name, ptr, BRC_STRLEN);
 	ptr += BRC_STRLEN;
-	int num = (*ptr++) & 0xff;
-	char *tmp = ptr + num * sizeof(brc_item_t);
-	if (num > BRC_MAXNUM)
-		num = BRC_MAXNUM;
-	*pnum = num;
-	memcpy(list, ptr, num * sizeof(brc_item_t));
+	brc_size_t size = *ptr++;
+	char *tmp = ptr + size * sizeof(brc_item_t);
+	if (size > BRC_MAXNUM)
+		size = BRC_MAXNUM;
+	*psize = size;
+	memcpy(list, ptr, size * sizeof(brc_item_t));
 	return tmp;
 }
 
@@ -62,14 +64,13 @@ void brc_update(const char *userid, const char *board)
 	char tmp_buf[BRC_MAXSIZE], *tmp;
 	char tmp_name[BRC_STRLEN];
 	brc_item_t tmp_list[BRC_MAXNUM];
-	int tmp_num;
 	int fd, tmp_size;
 	if (!brc.changed) {
 		return;
 	}
 	ptr = brc_buf.ptr;
-	if (brc.num > 0) {
-		ptr = brc_put_record(ptr, brc.name, brc.num, brc.list);
+	if (brc.size) {
+		ptr = brc_put_record(ptr, brc.name, brc.size, brc.list);
 	}
 	if (1) {
 		sethomefile(dirfile, userid, ".boardrc");
@@ -84,12 +85,13 @@ void brc_update(const char *userid, const char *board)
 	}
 	tmp = tmp_buf;
 	while (tmp < &tmp_buf[tmp_size] && (*tmp >= ' ' && *tmp <= '~')) {
-		tmp = brc_get_record(tmp, tmp_name, &tmp_num, tmp_list);
+		brc_size_t tmp_size;
+		tmp = brc_get_record(tmp, tmp_name, &tmp_size, tmp_list);
 		if (strncmp(tmp_name, board, BRC_STRLEN) != 0) {
-			ptr = brc_put_record(ptr, tmp_name, tmp_num, tmp_list);
+			ptr = brc_put_record(ptr, tmp_name, tmp_size, tmp_list);
 		}
 	}
-	brc_buf.size = (int) (ptr - brc_buf.ptr);
+	brc_buf.size = (brc_size_t) (ptr - brc_buf.ptr);
 
 	if ((fd = open(dirfile, O_WRONLY | O_CREAT, 0644)) != -1) {
 		ftruncate(fd, 0);
@@ -123,15 +125,15 @@ int brc_init(const char *uname, const char *bname)
 
 	char *ptr = brc_buf.ptr, *end = brc_buf.ptr + brc_buf.size;
 	while (ptr < end && (*ptr >= ' ' && *ptr <= '~')) {
-		ptr = brc_get_record(ptr, brc.name, &brc.num, brc.list);
+		ptr = brc_get_record(ptr, brc.name, &brc.size, brc.list);
 		if (strneq(brc.name, bname, BRC_STRLEN)) {
-			return brc.num;
+			return brc.size;
 		}
 	}
 
 	strlcpy(brc.name, bname, sizeof(brc.name));
 	brc.list[0] = 1;
-	brc.num = 1;
+	brc.size = 1;
 	return 0;
 }
 
@@ -150,19 +152,19 @@ void brc_mark_as_read(int64_t id)
 {
 	int r = (int)id;
 
-	if (brc.num <= 0) {
-		brc.list[brc.num++] = r;
+	if (!brc.size) {
+		brc.list[brc.size++] = r;
 		brc.changed = true;
 		return;
 	}
 
-	for (int i = 0; i < brc.num; ++i) {
+	for (int i = 0; i < brc.size; ++i) {
 		if (r == brc.list[i]) {
 			return;
 		} else if (r > brc.list[i]) {
-			if (brc.num < BRC_MAXNUM)
-				brc.num++;
-			for (int j = brc.num - 1; j > i; --j) {
+			if (brc.size < BRC_MAXNUM)
+				brc.size++;
+			for (int j = brc.size - 1; j > i; --j) {
 				brc.list[j] = brc.list[j - 1];
 			}
 			brc.list[i] = r;
@@ -170,8 +172,8 @@ void brc_mark_as_read(int64_t id)
 			return;
 		}
 	}
-	if (brc.num < BRC_MAXNUM) {
-		brc.list[brc.num++] = r;
+	if (brc.size < BRC_MAXNUM) {
+		brc.list[brc.size++] = r;
 		brc.changed = true;
 	}
 }
@@ -189,10 +191,10 @@ bool brc_unread(int64_t id)
 {
 	int r = (int)id;
 
-	if (brc.num <= 0)
+	if (!brc.size)
 		return true;
 
-	for (int i = 0; i < brc.num; ++i) {
+	for (int i = 0; i < brc.size; ++i) {
 		if (r > brc.list[i])
 			return true;
 		else if (r == brc.list[i])
@@ -210,14 +212,14 @@ bool brc_unread_legacy(const char *filename)
 
 int brc_first_unread(void)
 {
-	if (brc.num > 0 && brc.num <= BRC_MAXNUM)
-		return brc.list[brc.num - 1] + 1;
+	if (brc.size && brc.size <= BRC_MAXNUM)
+		return brc.list[brc.size - 1] + 1;
 	return 1;
 }
 
 int brc_last_read(void)
 {
-	if (brc.num > 0)
+	if (brc.size)
 		return brc.list[0];
 	return 0;
 }
@@ -235,7 +237,7 @@ void brc_clear_all(int bid)
 
 void brc_zapbuf(int *zbuf)
 {
-	if (*zbuf > 0 && brc.num > 0)
+	if (*zbuf > 0 && brc.size)
 		*zbuf = brc.list[0];
 }
 
